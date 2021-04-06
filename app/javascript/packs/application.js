a945e6eddd074f9b67fc945f01845af365ae4e8b
aacuadras <aaron-98@live.com.mx>
2021-04-06 17:02:22 -0500

    Redesigned product#index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import 'bootstrap'
+import "@fortawesome/fontawesome-free/js/all"
+import "../stylesheets/application"
+const images = require.context('../images', true)
